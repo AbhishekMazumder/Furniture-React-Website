f95a9dcc7efbe5acc40a6dad82190d36b2ae9a3a
abhishek <abhishekmazumder69@gmail.com>
2021-08-26 23:44:04 +0530

    AddFeaturedProduct section to Homepage and formatPrice

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,9 @@
-export const formatPrice = () => {}
+export const formatPrice = number => {
+	return new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'INR',
+		maximumFractionDigits: 2,
+	}).format(number / 100);
+};
 
 export const getUniqueValues = () => {}
